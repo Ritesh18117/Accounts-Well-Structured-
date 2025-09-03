cdce7abeecd84590cb8d31ed34de2aaebeffb5b2
Main <ritesh.gupta.18117@gmail.com>
2025-09-03 17:41:57 +0530

    Adding Auditor in the Accounts and Customer: - createdAt - createdBy - updatedAt - updatedBy

---
+++ src/main/java/com/project/accounts/audit/AuditAwareImpl.java
@@ -0,0 +1,19 @@
+package com.project.accounts.audit;
+
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component("auditAwareImpl")
+public class AuditAwareImpl implements   AuditorAware<String> {
+
+    /**
+     *
+     * @return the current Auditor
+     */
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("ACCOUNT_MS");
+    }
+}
