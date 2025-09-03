61ae65e9936243f2cb6ebb969f4b694e45921bdb
Main <ritesh.gupta.18117@gmail.com>
2025-09-03 23:53:08 +0530

    Adding Swagger Documentation view:  accessing /swagger-ui/index.html

--- src/main/java/com/project/accounts/dto/AccountsDto.java
+++ src/main/java/com/project/accounts/dto/AccountsDto.java
@@ -1,19 +1,34 @@
 package com.project.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
+@Schema(  // For Swagger documentation
+        name = "Accounts",
+        description = "Schema to hold Account Information"
+)
 @Data
 public class AccountsDto {
 
+    @Schema(  // For Swagger documentation
+            description = "Account Number of the Bank Account"
+    )
     @NotEmpty(message = "Account number must not be a null or empty")
     @Pattern(regexp = "(^$|[0-9]{10})", message = "Account number must be 10 digits")
     private Long accountNumber;
 
+    @Schema(  // For Swagger documentation
+            description = "Account Type of the Bank Account",
+            example = "Savings"
+    )
     @NotEmpty(message = "Account Type cannot be null or Empty")
     private String accountType;
 
+    @Schema(  // For Swagger documentation
+            description = "Branch Address of the Bank"
+    )
     @NotEmpty(message = "Branch Address cannot be null or Empty")
     private String branchAddress;
 
