66a8a800c1d037b183e3bc7a3d3e3aab1ae59360
Main <ritesh.gupta.18117@gmail.com>
2025-09-03 17:22:24 +0530

    Adding Validation in AccountsDto and CustomerDto

--- src/main/java/com/project/accounts/dto/AccountsDto.java
+++ src/main/java/com/project/accounts/dto/AccountsDto.java
@@ -1,13 +1,20 @@
 package com.project.accounts.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AccountsDto {
+
+    @NotEmpty(message = "Account number must not be a null or empty")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Account number must be 10 digits")
     private Long accountNumber;
 
+    @NotEmpty(message = "Account Type cannot be null or Empty")
     private String accountType;
 
+    @NotEmpty(message = "Branch Address cannot be null or Empty")
     private String branchAddress;
 
 }
